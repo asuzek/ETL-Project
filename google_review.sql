2a836fbc79a7b662e7ab3564d9b3983907fa3adf
asuzek <46697624+asuzek@users.noreply.github.com>
2019-04-15 20:53:12 -0500

    Add files via upload

--- google_review.sql
+++ google_review.sql
@@ -1,3 +1,10 @@
---Create and use google database
+-- Create and use google database
 CREATE DATABASE google_db; 
 USE google_db;
+CREATE TABLE google_review (
+     id int NOT NULL AUTO_INCREMENT,
+     overall_ratings float,
+    date DATE,
+    close float,
+    PRIMARY KEY (id)
+);
